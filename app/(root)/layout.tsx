ccf01890495c03baa5a45c4b7873d7c81fc68895
joye huang <huangdeshiou@gmail.com>
2025-04-24 16:34:26 +1000

    feat: home page ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,17 @@
 import { ReactNode } from 'react'
+import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
+    <div className="root-layout">
+      <nav>
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="logo" width={32} height={32} />
+          <h2 className="text-primary-100">PrepWise</h2>
+        </Link>
+      </nav>
       {children}
     </div>
   )
